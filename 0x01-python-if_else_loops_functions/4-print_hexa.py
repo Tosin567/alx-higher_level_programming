02eb77fd00f5b312e1788ca6047b10727183b738
Oluwatosin Jegede <oluwatosinjegede1@gmail.com>
2023-06-06 11:33:03 -0700

    Update

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-# For loop
-for x in range(0, 99):
-  print("Decimal: %d, Hexadecimal: %x" % (x, x))
+
+for num in range(0, 99):
+    print("{} = 0x{}".format(num, hex(num)[2:]), end="\n")
