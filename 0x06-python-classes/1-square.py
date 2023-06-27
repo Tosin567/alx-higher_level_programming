d9815735600584cfa5af6ccfd45e97a3a42dd470
Oluwatosin Jegede <oluwatosinjegede1@gmail.com>
2023-06-27 10:54:17 -0700

    Update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-class Square:
+"""this is a square. for square module"""
 
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
     """
-    # Write a class Square that defines a square by: (based on 0-square.py)
-    # Private instance attribute: size
-    # Instantiation with size (no type/value verification)
-    # VARIABLE(" "):
-    # class(Square): Square with size
-    # Return: Always 0. (Success)
-    """
-    """ Here, the '__init__' method is used as the constructor."""
+
     def __init__(self, size):
-        """ It takes a parameter 'size' and initializes the private instance...
-        ...attribute '__size' with the provided value"""
+        """Initializes the data."""
         self.__size = size
