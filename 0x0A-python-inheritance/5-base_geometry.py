ac4f1846b64eb9dd22a97703999c278c1de58843
Oluwatosin Jegede <oluwatosinjegede1@gmail.com>
2023-07-10 07:27:19 -0700

    Update

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,7 @@
-#!usr/bin/python3
-""" Empty class """
+#!/usr/bin/python3
+"""a module to write an empty class"""
 
 
 class BaseGeometry:
-    """ Empty class """
+    """an empty class"""
     pass
